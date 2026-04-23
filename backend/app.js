4de073bda392d342c50fb7d03577d101d02c5443
vishvjeetprajapati <vishvjeetprajapati138@gmail.com>
2026-04-23 18:07:54 +0530

    cors issue

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors({
-    origin: 'https://mb-bookverse.netlify.app/',
+    origin: 'https://mb-bookverse.netlify.app',
     credentials: true
 }));
 
